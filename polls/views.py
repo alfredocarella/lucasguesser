2decf83b47d771cea23fd8181499bcaa7c0474eb
Alfredo Carella <alfredocarella@gmail.com>
2018-01-30 21:19:03 +0100

    Add untested table and automatic winner

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from dateutil.parser import parse
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
@@ -6,12 +8,32 @@ import json
 # Create your views here.
 def index(request):
     with open("polls/guesstimates.json") as guess_list:
-        guesstimates = json.load(guess_list)
+        guess_by_person = json.load(guess_list)
 
     with open("polls/template.html", "r") as content_file:
         content = content_file.read()
-    response = content
-    # for key in guesstimates:
-    #     response += "{0} guessed {1}\n".format(key, guesstimates[key])
+
+    person_by_date = {parse(value):key for key, value in guess_by_person.items()}
+    dates = sorted(list(person_by_date.keys()))
+    best_guess = nearest(dates, datetime.today())
+    winner = person_by_date[best_guess]
+    response = content.format(winner, get_table(dates, person_by_date, best_guess))
     return HttpResponse(response)
+
+
+def nearest(items, pivot):
+    return min(items, key=lambda x: abs(x - pivot))
+
+
+def get_table(list_of_keys, dictionary, best_guess):
+    table = ""
+    for key in list_of_keys:
+        name = "<strong>"+dictionary[key]+"</strong>" if key == best_guess else dictionary[key]
+        date = "<strong>"+str(key)+"</strong>" if key == best_guess else key
+        table += """
+        <tr>
+        <td>{0}</td>
+        <td>{1}</td>
+        </tr>""".format(name, date)
+    return table
 
